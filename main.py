a0a2d7886dccef0ca288bea319baa8e7cc5bf934
Lass-Najmuldeen-nti <Lass.najmuldeen@elev.nti.ntig.se>
2024-04-26 13:13:50 +0200

    Commit, I added each subclass to its own file and tried to implement them into the main. Although, they could not be called upon. So, I had to remove them and bring them back.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 import sys
-import King, Rook
 
 # Initialize
 pygame.init()
@@ -38,6 +37,59 @@ class ChessPiece:
 
     def set_position(self, position):
         self.position = position
+
+
+class King(ChessPiece):
+    def __init__(self, color, image_path):
+        super().__init__('K', color, image_path)
+        self.position = None
+
+    def get_moves(self, board):
+        moves = []
+        current_row, current_col = self.position
+
+        # Define all possible directions for the King to move
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1),
+                      (1, 1), (-1, 1), (1, -1), (-1, -1)]
+
+        for dr, dc in directions:
+            new_row, new_col = current_row + dr, current_col + dc
+            if 0 <= new_row < 8 and 0 <= new_col < 8:
+                target_piece = board.get_piece_at(new_row, new_col)
+                if not target_piece or target_piece.color != self.color:
+                    moves.append((new_row, new_col))
+
+        return moves
+        pass  # TODO: Needs logic for the "castling" rule
+
+
+class Rook(ChessPiece):
+    def __init__(self, color, image_path):
+        super().__init__('R', color, image_path)
+        self.position = None
+
+    def get_moves(self, board):
+        moves = []
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]  # vertical and horizontal movement
+
+        current_row, current_col = self.position
+
+        for dr, dc in directions:
+            for i in range(1, 8):  # Rook can move up to 7 squares in any direction
+                new_row, new_col = current_row + i * dr, current_col + i * dc
+                if 0 <= new_row < 8 and 0 <= new_col < 8:
+                    target_piece = board.get_piece_at(new_row, new_col)
+                    if not target_piece:
+                        moves.append((new_row, new_col))
+                    elif target_piece.color != self.color:
+                        moves.append((new_row, new_col))
+                        break
+                    else:
+                        break
+                else:
+                    break
+        return moves
+        pass
 
 
 class Queen(ChessPiece):
